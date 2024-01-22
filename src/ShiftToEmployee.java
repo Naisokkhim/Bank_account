c0c40ae39eb472d17c9c692cd3c03d746a2900aa
Nai sokhim <mrkhimloy9@gmail.com>
2024-01-22 12:59:45 +0700

    V1

---
+++ src/ShiftToEmployee.java
@@ -0,0 +1,9 @@
+public class ShiftToEmployee extends EmlpoyeeInfomation{
+    private int start;
+    private int end;
+    private int maximum;
+
+    public ShiftToEmployee(String name, String preferred, String skill, int workHour) {
+        super(name, preferred, skill, workHour);
+    }
+}
